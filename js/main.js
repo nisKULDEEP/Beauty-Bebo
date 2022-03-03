8d69f735af20c01a3c799adb25a68123d898ff8d
Kuldeep Singh <kuldeepnishad.singh@gmail.com>
2022-03-03 10:49:39 +0530

    merge it

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+
+let home_page_category_component = document.getElementsByClassName("home_page_category_component");
+// console.log("djsfhj")
+console.log(home_page_category_component)
+home_page_category_component.addEventListener("hover");
