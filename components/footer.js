e2c073fee9f78d09a64c0dce4cf4e5ce8f33532e
Kuldeep Singh <kuldeepnishad.singh@gmail.com>
2022-03-04 16:08:53 +0530

    new page added

---
+++ components/footer.js
@@ -0,0 +1,139 @@
+function footer(){
+    return `<div class="footer-main-aj">
+    <div class="footer-top">
+      <div class="divide">
+        <div class="one">
+          <div class="footer-block-title">
+            <h3>CONTACT INFO</h3>
+          </div>
+          <div class="list-contact-aj">
+            <ul class="links-contact">
+              <li class="phone">(+91) 97990 30777</li>
+              <li class="mail">
+                <a href="mailto:support@beautybebo.com">support@beautybebo.com</a><br /><a
+                  href="mailto:sales@beautybebo.com"
+                  >sales@beautybebo.com
+                </a>
+              </li>
+              <li class="time">Open time: 10:00AM - 7:00PM</li>
+            </ul>
+          </div>
+          <div class="socials-wrap">
+            <ul class="links-contact">
+              <li class="li-social facebook-social">
+                <a
+                  title="Facebook"
+                  href="https://www.facebook.com/BeautyBebo-339715696750230/"
+                  target="_blank"
+                  ><span class="fa fa-facebook icon-social"></span
+                ></a>
+              </li>
+              <li class="li-social pinterest-social">
+                <a
+                  title="Instagram"
+                  href="https://www.instagram.com/beautybeboofficial/"
+                  target="_blank"
+                  ><span class="fa fa-instagram icon-social"></span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="two">
+          <div class="footer-block-title">
+            <h3>QUICK LINKS</h3>
+          </div>
+          <div class="footer-block-content">
+            <ul class="links">
+              <li><a href="/about-us">About us</a></li>
+              <li><a href="/contact">Contact Us</a></li>
+              <li><a href="/terms-and-conditions">Terms &amp; Conditions</a></li>
+              <li><a href="/privacy-policy">Privacy Policy</a></li>
+              <li><a href="/return-policy">Return and Refund Policy</a></li>
+              <li><a href="/shipping-policy">Shipping Policy</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="divide">
+        <div class="three">
+          <div class="footer-block-title">
+            <h3>CUSTOMER</h3>
+          </div>
+          <div class="footer-block-content">
+            <ul class="links">
+              <li><a href="/customer/account/">My Account</a></li>
+              <li><a href="/track-order/">Track your Order</a></li>
+              <li><a href="/order-return">Order Returns</a></li>
+              <li><a href="/wishlist/">Wishlist</a></li>
+              <li><a href="/blog/">News &amp; Events</a></li>
+              <li><a href="/faqs">FAQ</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="four">
+          <div class="footer-block-title">
+            <h3>CATEGORIES</h3>
+          </div>
+          <div class="footer-block-content categories">
+            <ul class="links">
+              <li>
+                <a href="https://www.beautybebo.com/makeup.html?___store=default"
+                  ><span>Makeup</span></a
+                >
+              </li>
+              <li>
+                <a href="https://www.beautybebo.com/skin.html?___store=default"
+                  ><span>Skin</span></a
+                >
+              </li>
+              <li>
+                <a href="https://www.beautybebo.com/hair.html?___store=default"
+                  ><span>Hair</span></a
+                >
+              </li>
+              <li>
+                <a href="https://www.beautybebo.com/personal-care.html?___store=default"
+                  ><span>Personal Care</span></a
+                >
+              </li>
+              <li>
+                <a href="https://www.beautybebo.com/mom-baby-care.html?___store=default"
+                  ><span>Mom &amp; Baby Care</span></a
+                >
+              </li>
+              <li>
+                <a href="https://www.beautybebo.com/fragrance.html?___store=default"
+                  ><span>Fragrance</span></a
+                >
+              </li>
+              <li>
+                <a href="https://www.beautybebo.com/ayurveda.html?___store=default"
+                  ><span>Ayurveda</span></a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom-1-aj">
+      <div class="footer-payment_img">
+        <img src="https://www.beautybebo.com/pub/media/wysiwyg/payment.png" alt="" />
+      </div>
+      <div class="footer-right-signup">
+        <div><span>Signup For Newsletter</span></div>
+        <div class="signup-buttons-place">
+          <div>
+            <input type="text" class="enter-signup-text" placeholder="Signup For Newsletter" />
+          </div>
+
+          <div><input type="button" value="Subscribe" class="enter-signup-button" /></div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom-2-aj">© 2020 All Rights Reserved.</div>
+  </div>`
+}
+export default footer;
--- js/main.js
+++ js/main.js
@@ -1,6 +1,1 @@
 
-
-let home_page_category_component = document.getElementsByClassName("home_page_category_component");
-// console.log("djsfhj")
-console.log(home_page_category_component)
-home_page_category_component.addEventListener("hover");
